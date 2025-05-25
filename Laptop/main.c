a947ad23faf3870b3f362ef71e4b4df666c8fab6
Vipaswi Thapa <vipa.thapa@gmail.com>
2025-05-25 13:42:41 -0400

    completion of main.

--- Laptop/main.c
+++ Laptop/main.c
@@ -1,22 +1,35 @@
-#pragma once
 #include "UDP_receiver.h"
 #include "imu_packet.h"
+#include "stdio.h"
 
-void main(){
+int main(){
   //setup
   startWSA();
   int sock = createSocket();
   int port = 8888;
-  bind_socket(sock, port);
+  if(bind_socket(sock, port) != 0){
+    perror("Socket binding failed");
+    return -1;
+  }
   
-
   int tracker = 0;
+  Quaternion* buffer = (Quaternion*) malloc(sizeof(Quaternion));
 
-  while(){
-
+  if(buffer == NULL){
+    perror("Buffer allocation failed");
+    return -1;
   }
 
-  //disconnect
+  while(1){
+    received_packet(sock, buffer, sizeof(Quaternion));
+    printf("Quaternion: w=%.2f x=%.2f y=%.2f z=%.2f\n", buffer->qw, buffer->qx, buffer->qy, buffer->qz);
+  }
+
+  free(buffer);
+  closeSocket(sock);
+  closeWSA();
+
+  return 0;
 }
 
 
